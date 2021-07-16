3bca08b81b0ac3f878e8ed45e41ea0ef23fa02f3
Felipe Blanco <45976931+felipe-blanco@users.noreply.github.com>
2021-07-15 19:56:54 -0600

    PDS-331: bump tomcat to 8.5.69
    - Attempted to bump to tomcat 8.5.68, but that resulted in errors when running core / Max Classic (issue with compilation of JSPs including tags - described here https://bz.apache.org/bugzilla/show_bug.cgi?id=65390 , and as noted in a comment on that thread, "Fixed in: 8.5.x for 8.5.69 onwards )
    - Bumped tomcat version in `pom.xml`
    - Added property `its.ajp.secret` to `pom.xml`, created corresponding option in `Tomcat8RunnerCli`
    - Setting secret in `AbstractRunMojo` and `Tomcat8Runner`
    - Modified assertions in `Tomcat8RunMultiConfigIT` and `Tomcat8SimpleWarProjectIT`
    -

--- tomcat8-war-runner/src/main/java/org/apache/tomcat/maven/runner/Tomcat8Runner.java
+++ tomcat8-war-runner/src/main/java/org/apache/tomcat/maven/runner/Tomcat8Runner.java
@@ -26,6 +26,8 @@ import org.apache.catalina.startup.Catalina;
 import org.apache.catalina.startup.ContextConfig;
 import org.apache.catalina.startup.Tomcat;
 import org.apache.catalina.valves.AccessLogValve;
+import org.apache.coyote.ajp.AbstractAjpProtocol;
+import org.apache.coyote.ajp.AjpNioProtocol;
 import org.apache.juli.ClassLoaderLogManager;
 import org.apache.tomcat.util.ExceptionUtils;
 import org.apache.tomcat.util.http.fileupload.FileUtils;
@@ -90,6 +92,8 @@ public class Tomcat8Runner
     public int maxPostSize = 2097152;
 
     public int ajpPort;
+
+    public String ajpSecret;
 
     public String serverXmlPath;
 
@@ -372,6 +376,10 @@ public class Tomcat8Runner
                 Connector ajpConnector = new Connector( "org.apache.coyote.ajp.AjpProtocol" );
                 ajpConnector.setPort( ajpPort );
                 ajpConnector.setURIEncoding( uriEncoding );
+                ajpConnector.setSecure( true );
+                AjpNioProtocol protocol= (AjpNioProtocol)ajpConnector.getProtocolHandler();
+                protocol.setSecret( ajpSecret );
+                protocol.setSecretRequired(true);
                 tomcat.getService().addConnector( ajpConnector );
             }
 
